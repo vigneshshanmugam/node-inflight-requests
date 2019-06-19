5a2fc8cca1bd9fb9c0cdc3bf66c5cd71abddd03d
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2019-06-19 18:02:55 +0200

    measure the inflight reqs

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const endOfStream = require("end-of-stream");
+
+module.exports = function congestion(opts) {
+  let inflightReqs = 0;
+  const error = opts.error || false;
+
+  function handler() {
+    inflightReqs--;
+    /** Handle edge case */
+    if (error && inflightReqs < 0) {
+      throw new Error(
+        `Inflight Requests is below minimum threshold - ${inflightReqs}`
+      );
+    }
+  }
+
+  return function(response) {
+    inflightReqs++;
+
+    endOfStream(response, handler);
+
+    return inflightReqs;
+  };
+};
